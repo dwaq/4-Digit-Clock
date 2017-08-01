6505cf874f133ce63670acf7fcd01c47866162b6
Dillon <ee.tinkerer@gmail.com>
2017-08-01 08:55:33 -0400

    add switch statement to segment

--- software/display.c
+++ software/display.c
@@ -1,6 +1,45 @@
 #include "display.h"
 
-int foo(int x)    /* Function definition */
+void display(int dig1, int dig2, int dig3, int dig4);
+void digit(int digit, int segment);
+
+// display a number using the 7 segments
+// number must be between 0-9
+// TODO: add range checks
+void segment(int number)
 {
-    return x + 5;
+  switch(number) {
+    case 0:
+
+      break;
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    case 5:
+
+      break;
+    case 6:
+
+      break;
+    case 7:
+
+      break;
+    case 8:
+
+      break;
+    case 9:
+
+      break;
+    default:
+      // bad - should't be here
+  }
 }
