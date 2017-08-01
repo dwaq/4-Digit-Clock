2553544e05bf7f8593850d47aebb677b72261a5e
Dillon <ee.tinkerer@gmail.com>
2017-08-01 09:17:11 -0400

    should write segments correctly

--- software/display.c
+++ software/display.c
@@ -18,36 +18,47 @@ void segment(int number)
   switch(number) {
     case 0:
       // A B C D E F
-
-
+      PORTB |= (1<<PORTB1 | 1<<PORTB0); // A F
+      PORTC |= (1<<PORTC3 | 1<<PORTC2); // D E
+      PORTD |= (1<<PORTD5 | 1<<PORTD0); // B C
       break;
     case 1:
       // B C
-
+      PORTD |= (1<<PORTD5 | 1<<PORTD0); // B C
       break;
     case 2:
-    // A B D E G
-
+      // A B D E G
+      PORTB |= (1<<PORTB1); // A
+      PORTC |= (1<<PORTC3 | 1<<PORTC2); // D E
+      PORTD |= (1<<PORTD5 | 1<<PORTD1); // B G
       break;
     case 3:
       // A B D E G
-
+      PORTB |= (1<<PORTB1); // A
+      PORTC |= (1<<PORTC3 | 1<<PORTC2); // D E
+      PORTD |= (1<<PORTD5 | 1<<PORTD1); // B G
       break;
     case 4:
       // B C F G
-
+      PORTB |= (1<<PORTB0); // F
+      PORTD |= (1<<PORTD5 | 1<<PORTD0 | 1<<PORTD1); // B C G
       break;
     case 5:
       // A C D F G
-
+      PORTB |= (1<<PORTB1 | 1<<PORTB0); // A F
+      PORTC |= (1<<PORTC3); // D
+      PORTD |= (1<<PORTD0 | 1<<PORTD1); // C G
       break;
     case 6:
       // A C D E F G
-
+      PORTB |= (1<<PORTB1 | 1<<PORTB0); // A F
+      PORTC |= (1<<PORTC3 | 1<<PORTC2); // D E
+      PORTD |= (1<<PORTD0 | 1<<PORTD1); // C G
       break;
     case 7:
       // A B C
-
+      PORTB |= (1<<PORTB1); // A
+      PORTD |= (1<<PORTD5 | 1<<PORTD0); // B C
       break;
     case 8:
       // A B C D E F G
@@ -57,7 +68,9 @@ void segment(int number)
       break;
     case 9:
       // A B C D F G
-
+      PORTB |= (1<<PORTB1 | 1<<PORTB0); // A F
+      PORTC |= (1<<PORTC3); // D
+      PORTD |= (1<<PORTD5 | 1<<PORTD0 | 1<<PORTD1); // B C G
       break;
     //default:
       // bad - should't be here
