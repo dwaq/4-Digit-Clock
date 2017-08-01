ef8376a30ac2c157574bfb2a1215dc40bcfbfd38
Dillon <ee.tinkerer@gmail.com>
2017-08-01 08:45:22 -0400

    define display pinout

--- software/display.h
+++ software/display.h
@@ -1,6 +1,26 @@
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
 
+// pinouts for common cathode 7 segment LED display:
+
+// segment is active high
+// segment | display | header | mcu
+// A  11 D9  PB1
+// B  7  D5  PD5
+// C  4  D0  PD0
+// D  2  A3  PC3
+// E  1  A2  PC2
+// F  10 D8  PB0
+// G  5  D1  PD1
+// DP 3  A4  PC4
+
+// digit is active low
+// digit | display | header | mcu
+// 1  12 D10  PB2
+// 2  9  D7   PD7
+// 3  8  D6   PD6
+// 4  6  D2   PD2
+
 int foo(int x);  /* An example function declaration */
 
 #endif // DISPLAY_H_
