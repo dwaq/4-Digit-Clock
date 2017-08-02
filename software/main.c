d8df3fbd1d1fba90a15f8ac4af235998f37396c2
Dillon <ee.tinkerer@gmail.com>
2017-08-02 08:52:44 -0400

    count up from 0-9 and flash decimal point

--- software/main.c
+++ software/main.c
@@ -4,6 +4,9 @@
 
 #include "display.h"
 
+int count = 0;
+int d = 0;
+
 int main(void)
 {
   // set pins to outputs
@@ -14,7 +17,31 @@ int main(void)
 
   while(1)
   {
-    // display all segments in loop so they keep updating
-    display(8,8,8,8);
+  	// display all segments in loop so they keep updating
+  	while (count < 25000)
+  	{
+      // half the time the dp is on
+  		if (count < 12500)
+  		{
+  			// decimal point
+  			display(d,d,1,d,d);
+  		}
+      // half the time it's off
+  		else
+  		{
+  			// no decimal point
+  			display(d,d,0,d,d);
+  		}
+  		count++;
+  	}
+  	count = 0;
+
+    // go to the next digit
+  	d++;
+    // can't display past 9
+  	if (d == 10)
+  	{
+  		d=0;
+  	}
   }
 }
