d51ae1158a85c1cb07ce0f1ec31c7f8ffb92e082
Dillon <ee.tinkerer@gmail.com>
2017-08-16 15:41:11 -0400

    move 1 sec timer init into new function

--- software/main.c
+++ software/main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#include "main.h"
 #include "display.h"
 
 // http://www.microchip.com//wwwAppNotes/AppNotes.aspx?appnote=en591232
@@ -18,21 +19,8 @@ int main(void)
   // set pins to outputs
   displaySetup();
 
-  // this code sets up timer1 for a 1s  @ 16Mhz Clock (mode 4)
-  // https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
-  
-  // Mode 4, CTC on OCR1A
-  OCR1A = 0x3D08;
-  TCCR1B |= (1 << WGM12);
-
-  //Set interrupt on compare match
-  TIMSK1 |= (1 << OCIE1A);  
-
-  // set prescaler to 1024 and start the timer
-  TCCR1B |= (1 << CS12) | (1 << CS10);
-
-  // enable interrupts
-  sei();
+  // setup timer for 1 second tick
+  timer1_init();
 
   while(1)
   {
@@ -41,12 +29,30 @@ int main(void)
   }
 }
 
+// this code sets up timer1 for a 1s  @ 16Mhz Clock (mode 4)
+// https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
+void timer1_init(void)
+{
+  // Mode 4, CTC on OCR1A
+  OCR1A = 0x3D08;
+  TCCR1B |= (1 << WGM12);
+
+  //Set interrupt on compare match
+  TIMSK1 |= (1 << OCIE1A);
+
+  // set prescaler to 1024 and start the timer
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+
+  // enable interrupts
+  sei();
+}
+
 // action to be done every 1 sec
 ISR (TIMER1_COMPA_vect)
 {
 	//keep track of time
 	// using 2 instead of 60 to make it faster
-	if (++sec==2)        
+	if (++sec==2)
 	{
 		sec=0;
 		// minutes
@@ -69,4 +75,3 @@ ISR (TIMER1_COMPA_vect)
 	// flip decimal point every second
 	dp ^= 1;
 }
-
